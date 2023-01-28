71514248163fff96d26bc520465a0472c091c4d4
RomanGorkavenko <roman@gorkavenko.ru>
2023-01-28 22:38:18 +0300

    initial commit

---
+++ HWsem1/Find.java
@@ -0,0 +1,35 @@
+package HWsem1;
+
+public class Find {
+    /**
+     * Найти такой индекс в массиве, что сумма элементов слева и справа равны.
+     * [1, 2, 5, 7, 12, 5, 3, 7] -> 4
+     */
+    public static int findCenter(int[] array) {
+        if (array == null || array.length <= 2) {
+            throw new IllegalArgumentException("Array must contains at least 3 elements");
+        }
+        for (int i = 1; i < array.length - 1; i++) {
+            int sum1 = 0;
+            int sum2 = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                sum1 += array[j];
+            }
+            for (int j = i + 1; j <= array.length - 1; j++) {
+                sum2 += array[j];
+
+            }
+            if (sum1 == sum2) {
+                return i;
+            }
+
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 5, 7, 12, 5, 3, 7};
+        System.out.println(findCenter(array));
+    }
+}
